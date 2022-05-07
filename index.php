93adeac5c619fe7d971decd459da8586c6135b80
Jose Bohorquez <bd567358546@gmail.com>
2022-05-07 10:48:20 -0500

    esta es una plantilla de inicio rapido con archivos de bootstrap 5 para produccion

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- bootstrap-Local -->
+    <link rel="stylesheet" href="Vistas/Css/bootstrap.min.css">
+    <script src="Vistas/Js/jquery-3.6.0.min.js"></script>
+    <script src="Vistas/Js/popper.min.js"></script>
+    <script src="Vistas/Js/bootstrap.min.js"></script>
+
+    <title>Bootstrap 5 Local</title>
+  </head>
+  <body>
+    <h1>Hola Jose Bienvenido A Esta Plantilla Rapida</h1>
+    <p>De bootstrap 5 el local listo para el desarrollo agil</p>
+
+
+
+  </body>
+</html>
