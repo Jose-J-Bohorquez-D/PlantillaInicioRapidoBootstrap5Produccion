d20597bfb0d21f0919b381082b0d6fa0c5821108
Jose Bohorquez <bd567358546@gmail.com>
2022-07-02 15:46:18 -0500

    subimos proyecto para test con servidor real

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php 
 
-    <!-- bootstrap-Local -->
-    <link rel="stylesheet" href="Vistas/Css/bootstrap.min.css">
-    <script src="Vistas/Js/jquery-3.6.0.min.js"></script>
-    <script src="Vistas/Js/popper.min.js"></script>
-    <script src="Vistas/Js/bootstrap.min.js"></script>
 
-    <title>Bootstrap 5 Local</title>
-  </head>
-  <body>
-    <h1>Hola Jose Bienvenido A Esta Plantilla Rapida</h1>
-    <p>De bootstrap 5 el local listo para el desarrollo agil</p>
+require_once("Controladores/Controlador.Plantilla.php");
+
+
+require_once("Modelos/Modelo.Plantilla.php");
+
+
+$mvc=new MvcControlador; $mvc->llamandoPlantilla();
 
 
 
-  </body>
-</html>
+ ?>
